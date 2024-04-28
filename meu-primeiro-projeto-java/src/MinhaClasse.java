ac325036008fca7f37379bf0e67eaf4cc360738e
RodTds <rodrigo.silva505@fatec.sp.gov.br>
2024-04-28 17:17:21 -0300

    Contuindo Função "Métodos"

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+   public static void  main(String[] args){
+    // declarações
+    String primeiroNome = "rodrigo";
+    String segundoNome ="Silva";
+    System.out.print(nomeCompleto(primeiroNome, segundoNome));
+  
+   }// fim da main
+     // métodos( função)
+   public static String nomeCompleto(String primeiroNome,String segundoNome){
+        return "Nome Completo: " + primeiroNome.concat(" ").concat(segundoNome);
+    }// fim da função somar 
+
+}// fim da minha classe geral
