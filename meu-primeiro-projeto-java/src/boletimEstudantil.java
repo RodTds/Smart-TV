210c81e755da0b328ed7b48eb177905364089356
RodTds <rodrigo.silva505@fatec.sp.gov.br>
2024-04-28 17:55:02 -0300

    aprendendo identação

---
+++ meu-primeiro-projeto-java/src/boletimEstudantil.java
@@ -0,0 +1,13 @@
+public class boletimEstudantil {
+
+public static void main(String[] args){
+    int mediaFinal = 10;
+    if(mediaFinal < 6)
+        System.out.println("Reprovado");
+     else if(mediaFinal == 6)
+        System.out.println("Prova Minerva");
+    else
+        System.out.println("Aprovado"); 
+      }// fim metodo aprova aluno
+    
+}
